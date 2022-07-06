4824d4c72316ecf890f908fe02815b7cf778becd
pravinkpawase <pravinpawase98@gmail.com>
2022-07-06 11:04:31 +0530

    Reverse Feature
    Feature Completed Please Review it
    Thanks

---
+++ GitHubProject/src/packPrograms/Reverse.java
@@ -0,0 +1,18 @@
+package packPrograms;
+
+public class Reverse {
+public static void main(String[] args) {
+	
+	int n= 456;
+	
+	int a=n%10;
+	System.out.print(a);
+	int b=n/10;
+	int c=b%10;
+	System.out.print(c);
+	int d=b/10;
+	System.out.println(d);
+	
+	
+}
+}
